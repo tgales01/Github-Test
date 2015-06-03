8ef5f7b143d6f47b024e31843f969b9152917c14
Travis Gales <Travis@Traviss-MacBook-Pro-2.local>
2015-06-03 16:18:30 -0700

    Initial commit

---
+++ Github Test/Github Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Github Test
+//
+//  Created by Travis Gales on 6/3/15.
+//  Copyright (c) 2015 Travis Gales. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Github Test/Github Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Github Test
+//
+//  Created by Travis Gales on 6/3/15.
+//  Copyright (c) 2015 Travis Gales. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
