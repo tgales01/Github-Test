ff8c25228d82f72c8d803a5bf9e48bc4e838e8d1
Travis Gales <Travis@Traviss-MacBook-Pro-2.local>
2015-06-03 16:24:47 -0700

    Added logging to app delegate and view controller

--- Github Test/Github Test/ViewController.m
+++ Github Test/Github Test/ViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"View Controller loaded!");
 }
 
 - (void)didReceiveMemoryWarning {
